56ca9cf81388a2a935f86eeddefc8c960d052462
renan <0renan.souza@gmail.com>
2017-04-28 01:55:48 -0300

    limitar width para visualizacao

--- client/index.jsx
+++ client/index.jsx
@@ -12,6 +12,8 @@ import normalize from 'normalize.css';
 import Structure from './assets/src/jsx/Structure.jsx';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Scrollbars } from 'react-custom-scrollbars';
+
 injectTapEventPlugin();
 
 import reducer from './assets/src/flux/reducers/index.js';
@@ -20,13 +22,26 @@ let store = createStore(reducer);
 class App extends Component {
 	constructor(props) {
 		super(props);
+
+		this.insertFakeMobile = false;
+	}
+
+	componentWillMount() {
+		let iFrame = document.getElementById("fake-mobile");
+		if(iFrame == null) {
+			this.insertFakeMobile = true;
+		}
+
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			document.getElementsByTagName("html")[0].className += " is-mobile";
+		}
 	}
 
 	render() {
 		return (
 			<div className="app-wrapper">
 				<Structure />
-			</div>	
+			</div>
 		);
 	}
 }
