2f41686f68d59ce5126d065de496918f52322845
Leonardo Carmona <leocarmona_@outlook.com>
2017-06-16 02:02:44 -0300

    Tentativa 2 openshift

--- server/src/main/java/lamba/agenda/api/AgendaApplication.java
+++ server/src/main/java/lamba/agenda/api/AgendaApplication.java
@@ -2,6 +2,8 @@ package lamba.agenda.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * 
@@ -16,10 +18,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class AgendaApplication {
+public class AgendaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AgendaApplication.class);
 	}
 
 }
