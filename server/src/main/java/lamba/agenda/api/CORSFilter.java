e2130cb1b2d328f786d7e4fdee36156727f378b0
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-06-18 00:20:59 -0300

    Inclusão de filtro de cors

---
+++ server/src/main/java/lamba/agenda/api/CORSFilter.java
@@ -0,0 +1,66 @@
+package lamba.agenda.api;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * 
+ * @author Leonardo Carmona da Silva
+ *         <ul>
+ *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
+ *         <li><a href="https://github.com/leocarmona">https://github.com/leocarmona</a></li>
+ *         <li><a href="mailto:lcdesenv@gmail.com">lcdesenv@gmail.com</a></li>
+ *         </ul>
+ *
+ */
+@Component
+public class CORSFilter implements Filter {
+
+	private final Logger log = LoggerFactory.getLogger(CORSFilter.class);
+
+	public CORSFilter() {
+		log.info("SimpleCORSFilter init");
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		log.info(req.toString());
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
